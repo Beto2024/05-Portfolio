c5b414c3cf4d4c5bfab050975e17ebee65950cb7
Beto2024 <156013866+Beto2024@users.noreply.github.com>
2024-12-01 16:15:10 -0300

    first commit

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Criado em novembro de 2024
+        </footer>
+    )
+}
+
+export default Footer
